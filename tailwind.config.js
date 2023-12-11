988e8468ef683b752a206980fccfb33a7a81cdd3
Tactenator <trevor.mclaurine@gmail.com>
2023-12-11 00:13:50 -0600

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", 
+    './**.{html,js}'
+  ],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'background': '#fafafa'
+      },
+      colors: {
+        'text': '#260d0d',
+        'background': '#fafafa',
+        'primary': '#c24c4c',
+        'secondary': '#eee6e2',
+        'accent': '#a73939',
+        'blurb': '#f2f2f2', 
+        'link': '#2200CC'
+       },
+       backgroundImage: {
+        'hero-1': "./public/img/bg-1.jpg"
+       }
+       
+    },
+  },
+  plugins: [],
+}
