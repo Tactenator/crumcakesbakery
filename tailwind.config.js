4777e63d83c9aa774d172d4a20f96ae984fa1645
Trevor McLaurine <trevor.mclaurine@gmail.com>
2024-01-25 21:28:05 -0600

    trying to fix weird width bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,43 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}", 
-    './**.{html,js}'
-  ],
+  content: ["./src/**/*.{html,js}", "./**.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        sm: "640px",
+        // => @media (min-width: 640px) { ... }
+
+        md: "768px",
+        // => @media (min-width: 768px) { ... }
+
+        lg: "1024px",
+        // => @media (min-width: 1024px) { ... }
+
+        xl: "1280px",
+        // => @media (min-width: 1280px) { ... }
+
+        "2xl": "1536px",
+        // => @media (min-width: 1536px) { ... }
+      },
       backgroundColor: {
-        'background': '#fafafa'
+        background: "#fafafa",
       },
       colors: {
-        'text': '#090608',
-        'background': '#fdfcfd',
-        'primary': '#f782b9',
-        'secondary': '#F3F782',
-        'accent': '#82F7C1', 
-        'link': '#b2e4e8',
-        'footer': "#282c2f"
-       },
-       backgroundImage: {
-        'hero-1': "./public/img/bg-1.jpg"
-       },
-       fontFamily: {
-        'fredoka': ['Fredoka', 'sans-serif']
+        text: "#090608",
+        background: "#fdfcfd",
+        primary: "#f782b9",
+        secondary: "#F3F782",
+        accent: "#82F7C1",
+        link: "#b2e4e8",
+        footer: "#282c2f",
       },
-       
+      backgroundImage: {
+        "hero-1": "./public/img/bg-1.jpg",
+      },
+      fontFamily: {
+        fredoka: ["Fredoka", "sans-serif"],
+      },
     },
   },
   plugins: ["prettier-plugin-tailwindcss"],
-}
+};
